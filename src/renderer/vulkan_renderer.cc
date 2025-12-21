9a6961e9d3026df5947d5b2909004b4363819701
Melwan Chevassus <melwan.chevassus@epita.fr>
2025-12-21 17:49:55 +0100

    feat(renderer: vulkan): add color to debug callback

--- src/renderer/vulkan_renderer.cc
+++ src/renderer/vulkan_renderer.cc
@@ -4,8 +4,11 @@
 #include <vulkan/vulkan_core.h>
 
 #include <algorithm>
+#include <chrono>
 #include <iostream>
 #include <map>
+
+#include <io/escapes/ansi-escapes.hh>
 
 static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
     VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
@@ -26,11 +29,47 @@ static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
             { VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT, "VALIDATION" },
             { VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT, "PERFORMANCE" }
         };
+    static std::map<VkDebugUtilsMessageSeverityFlagBitsEXT, int>
+        severityToColor = {
+            { VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT, 27 },
+            { VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT, 46 },
+            { VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT, 202 },
+            { VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT, 88 }
+        };
 
-    std::cerr << "validation layer: [" << severityToString.at(messageSeverity)
-              << "][" << messageTypeToString.at(messageType) << "] "
-              << callbackData->pMessage;
-    std::cerr << std::endl;
+    std::chrono::system_clock::time_point now =
+        std::chrono::system_clock::now();
+    std::time_t currentTime = std::chrono::system_clock::to_time_t(now);
+    std::tm local_time = *std::localtime(&currentTime);
+    auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(
+                  now.time_since_epoch())
+        % 1000;
+    std::ostringstream oss;
+    oss << std::put_time(&local_time, "%Y-%m-%d %H:%M:%S");
+
+    std::cout << FG_ESC(244) << "[" << oss.str() << "."
+              << std::format("{:03}", ms.count()) << "]";
+    switch (messageSeverity)
+    {
+    case VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT:
+        std::cout << FG_ESC(27);
+        break;
+    case VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT:
+        std::cout << FG_ESC(46);
+        break;
+    case VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT:
+        std::cout << FG_ESC(202);
+        break;
+    case VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT:
+        std::cout << FG_ESC(88);
+        break;
+    default:
+        std::cout << FG_ESC(15);
+        break;
+    }
+    std::cout << "[" << severityToString.at(messageSeverity) << "]"
+              << FG_ESC(15) << "[" << messageTypeToString.at(messageType)
+              << "] " << ESC_RESET << callbackData->pMessage << std::endl;
     return VK_FALSE;
 }
 
