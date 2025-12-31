6b8d3f699def92cb4b445ee4ada4484834cde1b7
Melwan Chevassus <melwan.chevassus@epita.fr>
2025-12-31 10:30:23 +0100

    feat(renderer: vulkan): add multisampling

--- src/renderer/vulkan-renderer.cc
+++ src/renderer/vulkan-renderer.cc
@@ -616,4 +616,13 @@ void VulkanRenderer::createGraphicsPipeline()
     rasterizer.depthBiasClamp = 0.0f;
     rasterizer.depthBiasSlopeFactor = 0.0f;
 
+    VkPipelineMultisampleStateCreateInfo multisampling{};
+    multisampling.sType = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO;
+    multisampling.sampleShadingEnable = VK_FALSE;
+    multisampling.rasterizationSamples = VK_SAMPLE_COUNT_1_BIT;
+    multisampling.minSampleShading = 1.0f;
+    multisampling.pSampleMask = nullptr;
+    multisampling.alphaToCoverageEnable = VK_FALSE;
+    multisampling.alphaToOneEnable = VK_FALSE;
+
 }
