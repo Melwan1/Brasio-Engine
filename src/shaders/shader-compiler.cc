4a884ba4ba1666be1b4d5917a62c852e7110881f
Melwan Chevassus <melwan.chevassus@epita.fr>
2026-02-06 14:58:50 +0100

    fix(shaders): recompile shaders only if needed

--- src/shaders/shader-compiler.cc
+++ src/shaders/shader-compiler.cc
@@ -1,17 +1,23 @@
+#include <filesystem>
 #include <shaders/shader-compiler.hh>
 
 #include <fstream>
 #include <iostream>
+#include <chrono>
 
-ShaderCompiler::ShaderCompiler(const fs::path &baseShaderDirectoryPath, const fs::path &logPath)
+ShaderCompiler::ShaderCompiler(const fs::path &baseShaderDirectoryPath,
+                               const fs::path &logPath)
     : _baseShaderDirectoryPath(baseShaderDirectoryPath)
     , _logPath(logPath)
 {}
 
-std::pair<const fs::path, const fs::path> ShaderCompiler::getEntryPaths(const fs::path &entry)
+std::pair<const fs::path, const fs::path>
+ShaderCompiler::getEntryPaths(const fs::path &entry)
 {
     const fs::path destDirectoryPath("compiled-shaders/");
-    const fs::path destPath = (destDirectoryPath / fs::relative(entry, _baseShaderDirectoryPath)).replace_extension(".spv");
+    const fs::path destPath =
+        (destDirectoryPath / fs::relative(entry, _baseShaderDirectoryPath))
+            .replace_extension(".spv");
     return { entry, destPath };
 }
 
@@ -19,11 +25,26 @@ bool ShaderCompiler::compileShader(const fs::path &shaderPath)
 {
     const fs::path destDirectoryPath("compiled-shaders/");
     const fs::path resolvedPath = _baseShaderDirectoryPath / shaderPath;
-    const fs::path destPath = (destDirectoryPath / fs::relative(resolvedPath, _baseShaderDirectoryPath)).replace_extension(".spv");
+    const fs::path destPath =
+        (destDirectoryPath
+         / fs::relative(resolvedPath, _baseShaderDirectoryPath))
+            .replace_extension(".spv");
     fs::create_directories(destPath.parent_path());
 
+    // check if destination file is more recent than source file
+
+    if (fs::exists(destPath)
+        && fs::last_write_time(destPath) >= fs::last_write_time(resolvedPath))
+    {
+        // shader does not need to be recompiled
+        std::cout << "Shader at " << resolvedPath.string()
+                  << " does not need to be recompiled.\n";
+        return true;
+    }
+
     std::ostringstream commandStream;
-    commandStream << "glslc " << resolvedPath << " -o " << destPath << " 2>" << _logPath;
+    commandStream << "glslc " << resolvedPath << " -o " << destPath << " 2>"
+                  << _logPath;
     int returnCode = system(commandStream.str().c_str());
     if (!returnCode)
     {
@@ -36,6 +57,8 @@ bool ShaderCompiler::compileShader(const fs::path &shaderPath)
     ifs.seekg(0);
     ifs.read(fileContent.data(), fileSize);
 
-    std::cerr << "Compilation failed for shader " << shaderPath << "(return code " << returnCode << "):\n" << fileContent;
+    std::cerr << "Compilation failed for shader " << shaderPath
+              << "(return code " << returnCode << "):\n"
+              << fileContent;
     return false;
 }
