36c89163153013f2f9249ccabdf08f0bea241e7a
Melwan Chevassus <melwan.chevassus@epita.fr>
2026-03-14 12:10:54 +0100

    fix(renderer: vulkan): make DebugMessenger inherit core::Handler

--- include/renderer/vulkan/debug-messenger.hh
+++ include/renderer/vulkan/debug-messenger.hh
@@ -2,17 +2,14 @@
 
 #include <vulkan/vulkan_core.h>
 
+#include <core/handler.hh>
+
 namespace brasio::renderer::vulkan
 {
-    class DebugMessenger
+    class DebugMessenger : public core::Handler<VkDebugUtilsMessengerEXT>
     {
     public:
         DebugMessenger(const VkInstance &instance,
                        const VkDebugUtilsMessengerCreateInfoEXT &createInfo);
-        ~DebugMessenger();
-
-    private:
-        VkInstance _instance;
-        VkDebugUtilsMessengerEXT _messenger;
     };
 } // namespace brasio::renderer::vulkan
